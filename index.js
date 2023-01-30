678a800d7d6b2d53f3b7d5cbac06fca6b900f1a2
Teguh Dwi Cahya Kusuma <teguh.dc.kusuma@gmail.com>
2023-01-30 13:23:21 +0700

    update: origin

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
       "https://nullstudio.id/",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
   })
 );
 app.use(express.urlencoded({ extended: false }));
